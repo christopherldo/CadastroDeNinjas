40fbcbe09f4723c159f7b8fc1e9b79139ccf6071
Christopher Leonardo de Oliveira <christopherldo@gmail.com>
2025-04-24 18:42:09 -0400

    Add initial Controller class with welcome endpoint

---
+++ src/main/java/dev/java10x/cadastrodeninjas/Controller.java
@@ -0,0 +1,14 @@
+package dev.java10x.cadastrodeninjas;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/")
+    public String welcome() {
+        return new String("Hello, World!");
+    }
+}
