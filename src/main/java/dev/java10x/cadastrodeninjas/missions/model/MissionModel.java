324ba633c91b760b9a225e60c178f327e89bff56
Christopher Leonardo de Oliveira <christopherldo@gmail.com>
2025-04-29 08:30:07 -0400

    Add Lombok annotations to MissionModel and NinjaModel for boilerplate reduction

--- src/main/java/dev/java10x/cadastrodeninjas/missions/model/MissionModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/missions/model/MissionModel.java
@@ -10,9 +10,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "missions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissionModel {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -23,40 +29,4 @@ public class MissionModel {
 
     @ManyToMany(mappedBy = "missions")
     private List<NinjaModel> ninjas;
-
-    protected MissionModel() {}
-
-    public MissionModel(String name, MissionDifficulty difficulty) {
-        this.name = name;
-        this.difficulty = difficulty;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public MissionDifficulty getDifficulty() {
-        return difficulty;
-    }
-
-    public void setDifficulty(MissionDifficulty difficulty) {
-        this.difficulty = difficulty;
-    }
-
-    public List<NinjaModel> getNinjas() {
-        return ninjas;
-    }
-
-    @Override
-    public String toString() {
-        return "MissionModel [id=" + id + ", name=" + name + ", difficulty=" + difficulty + "]";
-    }    
 }
--- src/main/java/dev/java10x/cadastrodeninjas/ninjas/model/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/ninjas/model/NinjaModel.java
@@ -12,9 +12,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "ninjas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -27,42 +33,6 @@ public class NinjaModel {
     @ManyToMany
     private List<MissionModel> missions;
 
-    protected NinjaModel() {}
-
-    public NinjaModel(String name, String email, LocalDate birthDate) {
-        this.name = name;
-        this.email = email;
-        this.birthDate = birthDate;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
     public int getIdade() {
         if (this.birthDate == null) {
             throw new IllegalStateException("Data de nascimento não definida para calcular a idade.");
@@ -71,14 +41,5 @@ public class NinjaModel {
         LocalDate dataAtual = LocalDate.now(ZoneId.systemDefault());
         Period periodo = Period.between(this.birthDate, dataAtual);
         return periodo.getYears();
-    }
-
-    public List<MissionModel> getMissions() {
-        return missions;
-    }
-
-    @Override
-    public String toString() {
-        return "NinjaModel [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + "]";
     }
 }
